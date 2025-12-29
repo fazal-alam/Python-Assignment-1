c79f03a421f21a865b7e4f026defed2058a61f61
Fazal Alam <fazalalam0705@gmail.com>
2025-12-29 22:05:06 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,17 @@
+# Task 1: Perform Basic Mathematical Operations
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print("The sum is: ", addition)
+print("The subtraction is: ", subtraction)
+print("The multiplication is: ", multiplication)
+print("The division is: ", division)
+
+#Task 2: Create a Personalized Greeting
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name = first_name + " " + last_name
+print("Hello," , full_name, "!", "Welcome to the Python program.")
